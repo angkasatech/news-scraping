146644aa4dd0d4e8fe743c9f391d590f65e2afda
Ramadhan Adi P <ramadhanadiputra1@gmail.com>
2020-11-01 00:34:43 +0700

    detik.com scraper floatrates scraper
    Signed-off-by: Ramadhan Adi P <ramadhanadiputra1@gmail.com>

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/detik_popular')
 def detik_popular():
@@ -20,8 +20,13 @@ def detik_popular():
     titles = popular_area.findAll(attrs={'class': 'media__title'})
     images = popular_area.findAll(attrs={'class': 'media__image'})
 
-    return render_template('index.html', images=images)
+    return render_template('detik-scraper.html', images=images)
 
+@app.route('/idr_rates')
+def idr_rates():
+    source = requests.get('http://www.floatrates.com/daily/idr.json')
+    json_data = source.json()
+    return render_template('idr_rates.html', datajs=json_data.values())
 
 if __name__ == '__main__':
     app.run(debug=True)
